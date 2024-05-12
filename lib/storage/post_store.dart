211925f14b774b3df2c1a805737399847cd1c9fa
kariyayo <mail.bati11@gmail.com>
2024-05-12 23:47:37 +0900

    implement to remove a post

--- lib/storage/post_store.dart
+++ lib/storage/post_store.dart
@@ -5,9 +5,13 @@ import 'package:chooshi/model/post.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PostStore {
+  String _keyByMonth({required int year, required int month}) {
+    return 'posts_${year.toString() + month.toString()}';
+  }
+
   Future<void> add(Post post) async {
     final dt = post.timestamp;
-    final key = 'posts_${dt.year.toString() + dt.month.toString()}';
+    final key = _keyByMonth(year: dt.year, month: dt.month);
     print('add: $key');
     final prefs = await SharedPreferences.getInstance();
     final jsonList = prefs.getStringList(key) ?? [];
@@ -16,7 +20,7 @@ class PostStore {
   }
 
   Future<List<Post>> fetchByYearMonth(Month month) async {
-    final key = 'posts_${month.year.toString() + month.month.toString()}';
+    final key = _keyByMonth(year: month.year, month: month.month);
     print('fetch: $key');
     final prefs = await SharedPreferences.getInstance();
     final jsonList = prefs.getStringList(key);
@@ -24,5 +28,21 @@ class PostStore {
       return [];
     }
     return jsonList.map((json) => Post.fromJson(jsonDecode(json))).toList();
+  }
+
+  Future<void> remove(Post post) async {
+    final dt = post.timestamp;
+    final month = Month(year: dt.year, month: dt.month);
+    final list = await fetchByYearMonth(month);
+    list.remove(post);
+    _put(month, list);
+    return;
+  }
+
+  Future<void> _put(Month month, List<Post> data) async {
+    final key = _keyByMonth(year: month.year, month: month.month);
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList(key, data.map((post) => jsonEncode(post)).toList());
+    return;
   }
 }
