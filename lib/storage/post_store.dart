6ede576931bf605f6e9df9d5f3e0005899f5b38f
kariyayo <mail.bati11@gmail.com>
2024-05-18 22:07:41 +0900

    create prefs in main.dart

--- lib/storage/post_store.dart
+++ lib/storage/post_store.dart
@@ -2,47 +2,46 @@ import 'dart:convert';
 
 import 'package:chooshi/model/month.dart';
 import 'package:chooshi/model/post.dart';
+import 'package:chooshi/storage/prefs_notifier.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+final postStoreProvider = Provider<PostStore>((ref) {
+  final prefs = ref.read(prefsProvider).prefs;
+  return PostStore(prefs);
+});
+
 class PostStore {
+  const PostStore(this._prefs);
+  final SharedPreferences _prefs;
+
   String _keyByMonth({required int year, required int month}) {
     return 'posts_${year.toString() + month.toString()}';
   }
 
-  Future<void> add(Post post) async {
+  void add(Post post) {
     final dt = post.timestamp;
     final key = _keyByMonth(year: dt.year, month: dt.month);
-    print('add: $key');
-    final prefs = await SharedPreferences.getInstance();
-    final jsonList = prefs.getStringList(key) ?? [];
-    await prefs.setStringList(key, [jsonEncode(post), ...jsonList]);
-    return;
+    final jsonList = _prefs.getStringList(key) ?? [];
+    _prefs.setStringList(key, [jsonEncode(post), ...jsonList]);
   }
 
-  Future<List<Post>> fetchByYearMonth(Month month) async {
+  List<Post> fetchByYearMonth(Month month) {
     final key = _keyByMonth(year: month.year, month: month.month);
-    print('fetch: $key');
-    final prefs = await SharedPreferences.getInstance();
-    final jsonList = prefs.getStringList(key);
+    final jsonList = _prefs.getStringList(key);
     if (jsonList == null) {
       return [];
     }
     return jsonList.map((json) => Post.fromJson(jsonDecode(json))).toList();
   }
 
-  Future<void> remove(Post post) async {
+  void remove(Post post) {
     final dt = post.timestamp;
     final month = Month(year: dt.year, month: dt.month);
-    final list = await fetchByYearMonth(month);
+    final list = fetchByYearMonth(month);
     list.remove(post);
-    _put(month, list);
-    return;
-  }
-
-  Future<void> _put(Month month, List<Post> data) async {
     final key = _keyByMonth(year: month.year, month: month.month);
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setStringList(key, data.map((post) => jsonEncode(post)).toList());
+    _prefs.setStringList(key, list.map((post) => jsonEncode(post)).toList());
     return;
   }
 }
